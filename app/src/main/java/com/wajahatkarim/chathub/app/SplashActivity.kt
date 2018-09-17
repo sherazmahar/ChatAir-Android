45ea7f28500daef99ba1a295aaef286b7dedd5a1
Wajahat Karim <wajahatkarim3@gmail.com>
2018-09-17 21:24:10 +0500

    Initial empty project

---
+++ app/src/main/java/com/wajahatkarim/chathub/app/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.wajahatkarim.chathub.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
